8eec28ac51b7252f42459bd9ec241b60069e831a
FranLsz <franlsz95develop@gmail.com>
2016-04-07 10:19:14 +0200

    Final

---
+++ SpPedidosProviderHostedWeb/Models/Pedidos.cs
@@ -0,0 +1,11 @@
+﻿namespace SpPedidosProviderHostedWeb.Models
+{
+    public class Pedidos
+    {
+        public string Cliente { get; set; }
+        public string Pedido { get; set; }
+        public int Unidades { get; set; }
+        public double Total { get; set; }
+        public int idProducto { get; set; }
+    }
+}
---
+++ SpPedidosProviderHostedWeb/Models/Productos.cs
@@ -0,0 +1,8 @@
+﻿namespace SpPedidosProviderHostedWeb.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
